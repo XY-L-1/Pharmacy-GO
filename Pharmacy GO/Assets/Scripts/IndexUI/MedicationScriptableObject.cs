4d7c802931857a5d4e32edbc253e4247dcf59c28
HoiTan <97975734+HoiTan@users.noreply.github.com>
2025-03-27 11:34:29 -0700

    Added The Medication CSV Importer
     The Medication CSV Importer reads medication data from a CSV file (upload from your local) and automatically creates Medication ScriptableObject assets. If there exist the assets with the same name, they will be replaced with new ones. It can also link medication images by matching names if those images live in Assets/Art/MedIndexImage.

--- Pharmacy GO/Assets/Scripts/IndexUI/MedicationScriptableObject.cs
+++ Pharmacy GO/Assets/Scripts/IndexUI/MedicationScriptableObject.cs
@@ -12,8 +12,6 @@ public class Medication : ScriptableObject
     public string pharmacologicClass;  
 
     [Header("Pharmacological Details")]
-    [TextArea]
-    public string information;      
     
     [TextArea]
     public string mechanismOfAction;
